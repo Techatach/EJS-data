4883a2bd71a96876e8044013a931c2fef5621b13
Techatach <teachatach@gmail.com>
2024-01-17 18:28:31 +0700

    first commit

---
+++ server/models/Customer.js
@@ -0,0 +1,39 @@
+// !mdbgum
+
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+const Schema = mongoose.Schema;
+const CustomerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  tel: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("Customer", CustomerSchema);
